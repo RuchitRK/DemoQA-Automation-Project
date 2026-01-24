f9eecad1338edabfde7ae40b22c6219256b13803
RuchitRK <ruchit2525@gmail.com>
2026-01-24 17:08:40 +0530

    Element- Radio button done

--- src/test/java/com/project/DemoQA/EleRadioBtn.java
+++ src/test/java/com/project/DemoQA/EleRadioBtn.java
@@ -15,9 +15,15 @@ public class EleRadioBtn {
 		//1. to click on Elements
 		driver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div[2]/div/div[1]")).click();
 		Thread.sleep(2000);
-		//1.3 to click on Check Box module
+		//1.3 to click on Radio button module
 		driver.findElement(By.xpath("//*[@id=\"item-2\"]/span")).click();
 		Thread.sleep(2000);
+		//1.3.1 to click on Yes radio btn
+		driver.findElement(By.xpath("//label[text()='Yes']")).click();
+		Thread.sleep(2000);
+		//1.3.2 to click on Impressive radio btn
+		driver.findElement(By.xpath("//label[text()='Impressive']")).click();
+		Thread.sleep(2000);
+		driver.quit();	
 	}
-
 }
