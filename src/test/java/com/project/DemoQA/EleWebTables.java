54e4780090625ff5c929e04c86bd07c9a1e5617d
RuchitRK <ruchit2525@gmail.com>
2026-01-26 12:40:05 +0530

    Element WebTables done

--- src/test/java/com/project/DemoQA/EleWebTables.java
+++ src/test/java/com/project/DemoQA/EleWebTables.java
@@ -1,8 +1,15 @@
 package com.project.DemoQA;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.time.Duration;
 import java.util.List;
 
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -29,6 +36,29 @@ public class EleWebTables {
 		driver.findElement(By.xpath("//*[@id=\"item-3\"]/span")).click();
 		Thread.sleep(2000);
 		clearTable(); //for deleting existing data
+		//Now for adding data
+		String excelPath = "src\\test\\resources\\Excel_Data_for_Web_Table_Testing.xlsx";
+		FileInputStream fis = new FileInputStream(new File(excelPath));
+		Workbook wb1 = new XSSFWorkbook(fis);
+		Sheet sheet = wb1.getSheetAt(0);
+		DataFormatter formatter = new DataFormatter();
+		int rowCount = sheet.getLastRowNum();
+		//System.out.println("Found " +rowCount);
+		
+		for (int i = 1; i < rowCount; i++) {
+			Row row = sheet.getRow(i);
+			String fName = formatter.formatCellValue(row.getCell(0));
+            String lName = formatter.formatCellValue(row.getCell(1));
+            String age = formatter.formatCellValue(row.getCell(2));
+            String email = formatter.formatCellValue(row.getCell(3));
+            String salary = formatter.formatCellValue(row.getCell(4));
+            String dept = formatter.formatCellValue(row.getCell(5));
+            
+            addNewEntry(fName, lName, email, age, salary, dept);
+            
+		}
+		wb1.close();
+		fis.close();
 	}
 	public static void clearTable() {
 		List<WebElement> deleteButtons = driver.findElements(By.xpath("//span[@title='Delete']"));
@@ -38,4 +68,19 @@ public class EleWebTables {
 			deleteButtons = driver.findElements(By.xpath("//span[@title='Delete']"));
 		}
 	}
+	public static void addNewEntry(String fName, String lName, String email, String age, String salary, String dept) {
+        // Click Add btn
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("addNewRecordButton"))).click();
+
+        // Filling Form in Add
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("firstName"))).sendKeys(fName);
+        driver.findElement(By.id("lastName")).sendKeys(lName);
+        driver.findElement(By.id("userEmail")).sendKeys(email);
+        driver.findElement(By.id("age")).sendKeys(age);
+        driver.findElement(By.id("salary")).sendKeys(salary);
+        driver.findElement(By.id("department")).sendKeys(dept);
+
+        // click Submit btn
+        driver.findElement(By.id("submit")).click();
+    }
 }
