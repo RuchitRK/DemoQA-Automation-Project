030fe406d9852f179b423a60534d5746394255a6
RuchitRK <ruchit2525@gmail.com>
2026-01-26 11:00:02 +0530

    Added Whole Elements module - Now WIP

---
+++ src/test/java/com/project/DemoQA/EleButtons.java
@@ -0,0 +1,23 @@
+package com.project.DemoQA;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class EleButtons {
+	public static void main(String[] args) throws Exception {
+		WebDriverManager.edgedriver().setup();
+		WebDriver driver = new EdgeDriver();
+		driver.get("https://demoqa.com");
+		Thread.sleep(2000);
+		//1. to click on Elements
+		driver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div[2]/div/div[1]")).click();
+		Thread.sleep(2000);
+		//1.5 to click on Buttons module
+		driver.findElement(By.xpath("//*[@id=\"item-5\"]/span")).click();
+		Thread.sleep(2000);
+	}
+
+}
