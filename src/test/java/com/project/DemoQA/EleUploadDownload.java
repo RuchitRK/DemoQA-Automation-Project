eff93255541796dbbc511e550472742134ff0759
RuchitRK <ruchit2525@gmail.com>
2026-01-27 19:08:05 +0530

    Elements - Upload & Download done

--- src/test/java/com/project/DemoQA/EleUploadDownload.java
+++ src/test/java/com/project/DemoQA/EleUploadDownload.java
@@ -1,8 +1,14 @@
 package com.project.DemoQA;
 
+import java.io.File;
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -16,8 +22,21 @@ public class EleUploadDownload {
 		driver.findElement(By.xpath("//*[@id=\"app\"]/div/div/div[2]/div/div[1]")).click();
 		Thread.sleep(2000);
 		//1.8 to click on Upload & Download module
-		driver.findElement(By.xpath("//*[@id=\"item-8\"]/span")).click();
+		//download
+		driver.findElement(By.xpath("//*[@id=\"item-7\"]/span")).click();
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		WebElement downloadBtn = driver.findElement(By.id("downloadButton"));
+		downloadBtn.click();
+		String dlPath = System.getProperty("user.home")+ "/Downloads/sampleFile.jpeg";
+		File dlFile = new File(dlPath);
 		Thread.sleep(2000);
+		//upload
+		String upFile = System.getProperty("user.dir") + "\\src\\test\\resources\\Excel_Data_for_Web_Table_Testing.xlsx";
+		WebElement uploadBtn = driver.findElement(By.id("uploadFile"));
+		uploadBtn.sendKeys(upFile);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("uploadedFilePath")));
+        String resultText = driver.findElement(By.id("uploadedFilePath")).getText();
+        System.out.println("Browser showed: " + resultText);
 	}
 
 }
